6d00a826076770c55b1f3fe50d35fa8c4b05b077
tasfia236 <tasfiatabassum300@gmail.com>
2024-12-12 19:47:40 +0600

    selete theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,10 +6,19 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'pink700': '#ffafca',
+      'pink300': '#ffc8dc',
+      'purple300': '#dee3ff',
+      'purple700': '#9c89b8',
+      'sky300': '#bde0fe',
+      'sky500': '#a2d2ff',
+      'sky700': '#90c9ff',
+    },
   },
   plugins: [
     require("daisyui"),
-    require("flowbite/plugin"), 
+    require("flowbite/plugin"),
   ],
 }
 
