fa6fab50aad8464bb939e21cd6ff70880ea8148f
tasfia236 <tasfiatabassum@gmail.com>
2024-12-14 14:16:32 +0000

    convert hash password

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 
 const router = express.Router();
@@ -6,18 +7,24 @@ const router = express.Router();
 router.post('/signup', async (req, res) => {
     const { username, email, photo, password } = req.body;
 
-    if(!username || !email || !photo || !password	|| username === '' || email === '' || photo === '' || password === '') {
+    if (!username || !email || !photo || !password || username === '' || email === '' || photo === '' || password === '') {
         return res.status(400).json({ message: 'All fields are required' });
     }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
 
     const newUser = new User({
         username,
         email,
         photo,
-        password,
+        password: hashedPassword,
     });
-    await newUser.save();
-    res.json('SignUp Seccessfully.')
+    try {
+        await newUser.save();
+        res.json('SignUp Seccessfully.')
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 
 export default router;
