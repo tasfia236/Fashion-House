88281340a4e2b3616127342270d052a8a6b0526c
tasfia236 <tasfiatabassum300@gmail.com>
2024-12-17 22:17:04 +0600

    GET all user

--- client/src/Layouts/MainLayout.jsx
+++ client/src/Layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Navbar from '../shared/Navbar'
+
 import { Outlet } from 'react-router-dom'
 import { Footer } from '../shared/Footer'
+import Navbar from '../Shared/Navbar'
 
 export const MainLayout = () => {
   return (
